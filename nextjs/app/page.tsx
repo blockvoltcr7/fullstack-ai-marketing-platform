fc3e6c0a767ff5b27f593ff26cf8a646f089d901
sami sabir-idrissi <samisabiridrissi@icloud.com>
2024-09-26 23:29:12 -0400

    first commit

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div className="bg-red-500 text-white">
+      <div>Hello World</div>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
+}
