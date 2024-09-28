2e0c6fe5e840ea333db79aca88713ef6d8eda379
sami sabir-idrissi <samisabiridrissi@icloud.com>
2024-09-28 16:51:19 -0400

    adding lecture doc on usestate and state variables

--- nextjs/components/Sidebar.tsx
+++ nextjs/components/Sidebar.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button"; // Ensure you import the Button component
 
 function Sidebar() {
-  const [isMobileOpen, setIsMobileOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(false);
-  const [isCollapsedMobile, setIsCollapsedMobile] = useState(false);
 
   // Effect to handle window resize
   useEffect(() => {
@@ -13,21 +14,25 @@ function Sidebar() {
   }, []);
 
   // Function to toggle the mobile sidebar
-  const toggleSidebar = () => {
-    setIsMobileOpen(!isMobileOpen);
-  };
+  const toggleSidebar = () => {};
 
   // Function to handle outside click
   const handleOutsideClick = () => {
     // Implement outside click handling logic
   };
 
+  /**when we open the side bar menu we want see an x, when we close the menu we want to see the hamburger menu */
+  const renderMenuIcon = (isOpen: boolean) => {
+    // Implement menu icon rendering logic
+    return isOpen ? "X" : "menu";
+  };
+
   return (
     <div>
       {/* Placeholder for mobile x toggle in the left side of screen */}
+      {isMobile && <Button variant="ghost">renderMenuIcon(isOpen)</Button>}
 
       {/* Placeholder for storing all components in nav */}
-
       <div>
         <h1>AI Marketing Platform</h1>
 
