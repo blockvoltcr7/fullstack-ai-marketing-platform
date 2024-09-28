e05d657c1ccb07541bd4cde2d638dba1a9b467fd
sami sabir-idrissi <samisabiridrissi@icloud.com>
2024-09-28 13:53:08 -0400

    add logic to pages to resolve vercel type error due to module not found

--- nextjs/app/(dashboard)/settings/loading.tsx
+++ nextjs/app/(dashboard)/settings/loading.tsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function LoadingPage() {
   return (
     <div>
-      <p className="mt-4 text-lg font-semibold text-gray-700">
-        Loading settings...
-      </p>
+      <p>Loading settings...</p>
     </div>
   );
 }
---
+++ nextjs/app/(dashboard)/templatess/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TemplatesPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <p className="text-2xl font-semibold text-gray-800">
+        Welcome to the Templates Page
+      </p>
+    </div>
+  );
+}
