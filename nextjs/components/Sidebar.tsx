13de70bbb75cc39205a8cf44faee6c5f5e82ef6e
sami sabir-idrissi <samisabiridrissi@icloud.com>
2024-09-28 22:18:10 -0400

    working on toggle between mobile and desktop

--- nextjs/components/Sidebar.tsx
+++ nextjs/components/Sidebar.tsx
@@ -2,9 +2,10 @@
 
 import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button"; // Ensure you import the Button component
-
+import { X, Menu } from "lucide-react";
+import { cn } from "@/lib/utils";
 function Sidebar() {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(false);
 
@@ -14,7 +15,19 @@ function Sidebar() {
   }, []);
 
   // Function to toggle the mobile sidebar
-  const toggleSidebar = () => {};
+  /**
+   * Toggles the sidebar state based on the device type.
+   * For mobile devices, it toggles the 'isOpen' state.
+   * For desktop devices, it toggles the 'isCollapsed' state.
+   * This function ensures different sidebar behaviors for mobile and desktop views.
+   */
+  const toggleSidebar = () => {
+    if (isMobile) {
+      setIsOpen((prevIsOpen) => !prevIsOpen);
+    } else {
+      setIsCollapsed((prevIsCollapsed) => !prevIsCollapsed);
+    }
+  };
 
   // Function to handle outside click
   const handleOutsideClick = () => {
@@ -24,13 +37,25 @@ function Sidebar() {
   /**when we open the side bar menu we want see an x, when we close the menu we want to see the hamburger menu */
   const renderMenuIcon = (isOpen: boolean) => {
     // Implement menu icon rendering logic
-    return isOpen ? "X" : "menu";
+    return isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />;
   };
 
   return (
     <div>
-      {/* Placeholder for mobile x toggle in the left side of screen */}
-      {isMobile && <Button variant="ghost">renderMenuIcon(isOpen)</Button>}
+      {/* Placeholder for mobile x toggle in the left side of screen
+      if isMobile is true and renderMenuIcon is true then render the button with the icon */}
+      {isMobile && (
+        <Button
+          variant="ghost"
+          onClick={toggleSidebar}
+          className={cn(
+            "fixed top-4 left-4 z-50 z-50 bg-transparent hover:bg-gray-100/50 backdrop-blur-sm",
+            isOpen && "top-4 left-4"
+          )}
+        >
+          {renderMenuIcon(isOpen)}
+        </Button>
+      )}
 
       {/* Placeholder for storing all components in nav */}
       <div>
