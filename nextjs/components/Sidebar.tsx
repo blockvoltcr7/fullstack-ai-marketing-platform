0985fc7f25c4be1b8d896a7e3a3f4bd0d9403190
sami sabir-idrissi <samisabiridrissi@icloud.com>
2024-10-12 13:09:46 -0400

    try fixing imports again

--- nextjs/components/Sidebar.tsx
+++ nextjs/components/Sidebar.tsx
@@ -4,10 +4,10 @@ import React, { useEffect, useRef, useState } from "react";
 import { Button } from "./ui/button";
 import { Menu, X } from "lucide-react";
 import { cn } from "@/lib/utils";
-import SidebarNav from "../components/SidebarNav";
-import SidebarToggle from "@/components/SidebarToggle";
+import SidebarNav from "./SidebarNav";
+import SidebarToggle from "./SidebarToggle";
 import { useUser } from "@clerk/nextjs";
-import UserProfileSection from "@/components/UserProfileSection";
+import UserProfileSection from "./UserProfileSection";
 
 const MOBILE_WINDOW_WIDTH_LIMIT = 1024;
 
